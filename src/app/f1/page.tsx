82ee0a96adf6d9bb6a32e1e4192804b068e9def5
Halima Ehsan <halimabutt33@gmail.com>
2024-09-16 18:09:01 +0500

    Intercepting Route conventions used

---
+++ src/app/f1/page.tsx
@@ -0,0 +1,13 @@
+import Card from "@/components/card";
+import Link from "next/link";
+
+export default function F1(){
+    return (
+    <>
+    <Card>F1 Page</Card>
+    <div>
+        <Link href="/f1/f2">F2</Link>
+    </div>
+    </>
+)
+}
